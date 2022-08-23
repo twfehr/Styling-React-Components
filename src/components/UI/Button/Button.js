808657ce44f0a6648dfab5aac09cde13ef7d746b
Travis <39714398+twfehr@users.noreply.github.com>
2022-08-23 18:37:49 -0400

    Use CSS Modules to style a button
    Changed syntax of import, renamed CSS file, chose CSS selector via an object of our CSS file.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,43 +1,12 @@
-// import React from "react";
-import styled from "styled-components";
-// import './Button.css';
+import React from "react";
+import styles from "./Button.module.css";
 
-//Instead of storing a functional component in this variable, we add a tagged template literal `` to trigger the button method from our object from our styled-components package. Button method will return new button component.
-const Button = styled.button`
-  /* mobile device width */
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  /* non-mobile devices */
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  // use ampersand symbol for pseudo-selectors
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
